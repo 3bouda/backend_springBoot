670101eef1b2086345d0ea484c2df4d0ab14de0a
3bouda <abdelaliiheb@gmail.com>
2022-03-23 03:31:26 +0100

    fixing delete

--- src/main/java/backend/hcvbackend/controller/Confiancecont.java
+++ src/main/java/backend/hcvbackend/controller/Confiancecont.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -45,4 +46,10 @@ public class Confiancecont {
 
         return confiancerepo.save(memberFromDB);
     } 
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("{id}")
+    void delete(@PathVariable String id){
+        Confiance confianceToDelete = confiancerepo.findById(id).orElseThrow(RuntimeException::new);
+        confiancerepo.delete(confianceToDelete);
+    }
 }
